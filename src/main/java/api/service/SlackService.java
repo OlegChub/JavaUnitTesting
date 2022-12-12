df94aa9da06d8e8928d59a166dee7c5a88f97c3c
Oleg_Chubryk <hoplla18@gmail.com>
2022-12-12 21:35:47 +0100

    add Slack hook

--- src/main/java/api/service/SlackService.java
+++ src/main/java/api/service/SlackService.java
@@ -4,6 +4,7 @@ import org.apache.http.HttpStatus;
 import org.json.simple.JSONObject;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.post;
 
 public class SlackService {
     public static void postNotification(String message) {
@@ -13,8 +14,8 @@ public class SlackService {
                 .header("Content-type", "application/json")
                 .body(request.toJSONString()).
                 when()
-                .post("https://hooks.slack.com/services/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")
+                .post("https://hooks.slack.com/services/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")
                 .then()
-                .statusCode(HttpStatus.SC_OK);
+                .log().all();
     }
 }
